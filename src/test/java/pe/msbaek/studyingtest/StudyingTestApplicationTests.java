dc02d0bd8908556b653cc07780c4aea80dc7581c
Myeongseok Baek <codetemplate@hanmail.net>
2023-09-26 13:31:15 +0900

    add socket server

--- src/test/java/pe/msbaek/studyingtest/StudyingTestApplicationTests.java
+++ src/test/java/pe/msbaek/studyingtest/StudyingTestApplicationTests.java
@@ -9,5 +9,4 @@ class StudyingTestApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
---
+++ src/test/java/pe/msbaek/studyingtest/java21/TraditionalSocketServer.java
@@ -0,0 +1,40 @@
+package pe.msbaek.studyingtest.java21;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.Executors;
+
+/**
+ * echo "hello" | nc localhost 9090
+ */
+public class TraditionalSocketServer {
+    public static void main(String[] args) throws IOException {
+        var executor = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        try(var serverSocket = new ServerSocket(9090)) {
+            var clientSocket = serverSocket.accept();
+            executor.submit(() -> {
+                try {
+                    handleRequest(clientSocket);
+                }
+                catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+    }
+
+    private static void handleRequest(Socket clientSocket) throws IOException {
+        var next = -1;
+        try(var baos = new ByteArrayOutputStream()) {
+            try (var in = clientSocket.getInputStream()) {
+                while((next = in.read()) != -1) {
+                    baos.write(next);
+                }
+            }
+            var data = baos.toString();
+            System.out.printf("request: %s\n", data);
+        }
+    }
+}
