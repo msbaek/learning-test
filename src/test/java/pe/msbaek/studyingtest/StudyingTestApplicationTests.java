77ab9180d6678cf4a8f7b6936ab45387305c75c2
Myeongseok Baek <codetemplate@hanmail.net>
2023-09-26 09:09:26 +0900

    project created with java21

---
+++ src/test/java/pe/msbaek/studyingtest/StudyingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.msbaek.studyingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
