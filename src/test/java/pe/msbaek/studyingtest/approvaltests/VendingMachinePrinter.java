40fead0a0f8e7b234c3d67ef505be5d3cad391d3
Myeongseok Baek <codetemplate@hanmail.net>
2023-10-06 11:30:49 +0900

    add VendingMachine related files

---
+++ src/test/java/pe/msbaek/studyingtest/approvaltests/VendingMachinePrinter.java
@@ -0,0 +1,26 @@
+package pe.msbaek.studyingtest.approvaltests;
+
+public class VendingMachinePrinter {
+    private final int columns;
+    private final VendingMachine machine;
+
+    public VendingMachinePrinter(VendingMachine machine) {
+        this.columns = 60;
+        this.machine = machine;
+    }
+
+    String print(){
+        return "VendingMachine\n"
+                // TODO: finish this
+                ;
+    }
+
+    /** Convenience function that lays out a name and a value at either ends of a fixed-width line.
+     * eg if you call it with name="Foo" value="Bar" it will return
+     * Foo                                       Bar
+     */
+    private String formatLineWithWhitespace(String name, String value){
+        int whitespaceSize = columns - name.length() - value.length();
+        return String.format("%s%s%s\n", name, " ".repeat(Math.max(0, whitespaceSize)), value);
+    }
+}
