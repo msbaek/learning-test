ca11ef3f7dacee290645cf26cc2760744cfe729e
Myeongseok Baek <codetemplate@hanmail.net>
2023-10-16 16:23:44 +0900

    add webtau

---
+++ src/main/java/pe/msbaek/studyingtest/webtau/Game.java
@@ -0,0 +1,36 @@
+package pe.msbaek.studyingtest.webtau;
+
+import java.math.BigDecimal;
+
+public class Game {
+    private Long id;
+    private String type;
+    private BigDecimal priceUsd;
+
+    public Game() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public BigDecimal getPriceUsd() {
+        return priceUsd;
+    }
+
+    public void setPriceUsd(BigDecimal priceUsd) {
+        this.priceUsd = priceUsd;
+    }
+}
