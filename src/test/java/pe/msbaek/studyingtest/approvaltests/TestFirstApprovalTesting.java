5d80fc07b01289bfcf097ccd946eb67dab5fe24f
Myeongseok Baek <codetemplate@hanmail.net>
2023-10-27 16:51:24 +0900

    make it pass

--- src/test/java/pe/msbaek/studyingtest/approvaltests/TestFirstApprovalTesting.java
+++ src/test/java/pe/msbaek/studyingtest/approvaltests/TestFirstApprovalTesting.java
@@ -28,6 +28,10 @@ public class TestFirstApprovalTesting {
     }
 
     private boolean isLeapYear(int year) {
+        if(year % 400 == 0)
+            return true;
+        if(year % 100 == 0)
+            return false;
         return year % 4 == 0;
     }
 }
