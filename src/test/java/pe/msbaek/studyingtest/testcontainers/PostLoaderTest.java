244e9128be76131a1d53a8548fe34bd61f111bf8
Myeongseok Baek <codetemplate@hanmail.net>
2023-12-03 10:17:27 +0900

    extract AbstractTestContainerTest

--- src/test/java/pe/msbaek/studyingtest/testcontainers/PostLoaderTest.java
+++ src/test/java/pe/msbaek/studyingtest/testcontainers/PostLoaderTest.java
@@ -2,25 +2,9 @@ package pe.msbaek.studyingtest.testcontainers;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.jdbc.JdbcConnectionDetails;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
+import pe.msbaek.studyingtest.AbstractTestContainerTest;
 
-@Testcontainers
-@SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class PostLoaderTest {
-    @Container
-    @ServiceConnection
-    static MySQLContainer mysql = new MySQLContainer("mysql:latest");
-
-    @Autowired
-    JdbcConnectionDetails jdbcConnectionDetails;
-
+class PostLoaderTest extends AbstractTestContainerTest {
     @Autowired PostDataLoader loader;
     @Autowired PostRepository repository;
 
