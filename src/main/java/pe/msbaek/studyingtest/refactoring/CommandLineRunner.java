b8338d8df0bb974eff9f796f2181d39ec0a576bc
Myeongseok Baek <codetemplate@hanmail.net>
2023-12-07 11:31:28 +0900

    extract method - countOrders

--- src/main/java/pe/msbaek/studyingtest/refactoring/CommandLineRunner.java
+++ src/main/java/pe/msbaek/studyingtest/refactoring/CommandLineRunner.java
@@ -3,6 +3,7 @@ package pe.msbaek.studyingtest.refactoring;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.Arrays;
 
@@ -18,21 +19,25 @@ public class CommandLineRunner {
             String filename = args[args.length - 1];
             boolean onlyCountReady = Arrays.asList(args).contains("-r");
 
-            File input = Paths.get(filename).toFile();
-            ObjectMapper objectMapper = new ObjectMapper();
-            Order[] orders = objectMapper.readValue(input, Order[].class);
-            if(onlyCountReady) {
-                System.out.println(Arrays.stream(orders)
-                        .filter(order -> "ready".equals(order.status()))
-                        .count());
-            }
-            else {
-                System.out.println(orders.length);
-            }
+            countOrders(filename, onlyCountReady);
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
             System.exit(-1);
+        }
+    }
+
+    private static void countOrders(String filename, boolean onlyCountReady) throws IOException {
+        File input = Paths.get(filename).toFile();
+        ObjectMapper objectMapper = new ObjectMapper();
+        Order[] orders = objectMapper.readValue(input, Order[].class);
+        if(onlyCountReady) {
+            System.out.println(Arrays.stream(orders)
+                    .filter(order -> "ready".equals(order.status()))
+                    .count());
+        }
+        else {
+            System.out.println(orders.length);
         }
     }
 }
