fad287fa3ac189e870a1ba15d890bc30c3d204f3
Myeongseok Baek <codetemplate@hanmail.net>
2023-12-07 11:40:10 +0900

    change to use data structure

--- src/main/java/pe/msbaek/studyingtest/refactoring/CommandLineRunner.java
+++ src/main/java/pe/msbaek/studyingtest/refactoring/CommandLineRunner.java
@@ -19,7 +19,7 @@ public class CommandLineRunner {
 
             CountOrders countOrders = parse(args);
 
-            System.out.println(countOrders(countOrders, countOrders.filename(), countOrders.onlyCountReady()));
+            System.out.println(countOrders(countOrders));
         }
         catch (Exception e) {
             System.out.println(e.getMessage());
@@ -36,11 +36,11 @@ public class CommandLineRunner {
     private record CountOrders(String filename, boolean onlyCountReady) {
     }
 
-    private static long countOrders(CountOrders countOrders, String filename, boolean onlyCountReady) throws IOException {
-        File input = Paths.get(filename).toFile();
+    private static long countOrders(CountOrders countOrders) throws IOException {
+        File input = Paths.get(countOrders.filename()).toFile();
         ObjectMapper objectMapper = new ObjectMapper();
         Order[] orders = objectMapper.readValue(input, Order[].class);
-        if(onlyCountReady) {
+        if(countOrders.onlyCountReady()) {
             return Arrays.stream(orders)
                     .filter(order -> "ready".equals(order.status()))
                     .count();
