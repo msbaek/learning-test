9b9a4dcc31357942e35ca4d3c9ddd53c71f4d276
Myeongseok Baek <codetemplate@hanmail.net>
2024-01-29 16:15:26 +0900

    primitive obsession - inline unnecessary test test createUser

--- src/test/java/pe/msbaek/studyingtest/primitiveobsession/UserNameTest.java
+++ src/test/java/pe/msbaek/studyingtest/primitiveobsession/UserNameTest.java
@@ -8,16 +8,12 @@ public class UserNameTest {
     @Test
     void should_throw_invalid_argument_exception_when_user_name_contains_whitespace() {
         assertThrows(IllegalArgumentException.class, () -> {
-            User user = createUser("msbaek ");
+            User user = new User("msbaek ");
         });
     }
 
     @Test
     void should_not_throw_exception_when_user_name_contains_no_whitespace() {
-        createUser("msbaek");
-    }
-
-    private static User createUser(String name) {
-        return new User(name);
+        new User("msbaek");
     }
 }
