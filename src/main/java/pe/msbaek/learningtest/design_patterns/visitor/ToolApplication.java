68fb23db9845fb06b2ec753aa1e4a862295e481c
Myeongseok Baek <codetemplate@hanmail.net>
2024-03-06 10:25:45 +0900

    apply double dispatch

--- src/main/java/pe/msbaek/learningtest/design_patterns/visitor/ToolApplication.java
+++ src/main/java/pe/msbaek/learningtest/design_patterns/visitor/ToolApplication.java
@@ -1,8 +1,5 @@
 package pe.msbaek.learningtest.design_patterns.visitor;
 
-import java.util.ArrayList;
-import java.util.List;
-
 abstract class ResourceFile {
     protected String filePath;
 
@@ -10,7 +7,7 @@ abstract class ResourceFile {
         this.filePath = filePath;
     }
 
-    public abstract void extract2txt();
+    public abstract void accept(Extractor extractor);
 }
 
 class PPTFile extends ResourceFile {
@@ -19,9 +16,8 @@ class PPTFile extends ResourceFile {
     }
 
     @Override
-    public void extract2txt() {
-        // PPT 형식의 파일에서 본문을 추출하는 코드
-        // 추출한 데이터는 동일한 경로에 같은 이름을 가진 TXT 파일에 저장 System.out.println("Extract PPT.");
+    public void accept(Extractor extractor) {
+        extractor.extract2txt(this);
     }
 }
 
@@ -31,8 +27,8 @@ class PdfFile extends ResourceFile {
     }
 
     @Override
-    public void extract2txt() {
-        System.out.println("Extract PDF.");
+    public void accept(Extractor extractor) {
+        extractor.extract2txt(this);
     }
 }
 
@@ -42,8 +38,8 @@ class WordFile extends ResourceFile {
     }
 
     @Override
-    public void extract2txt() {
-        System.out.println("Extract WORD.");
+    public void accept(Extractor extractor) {
+        extractor.extract2txt(this);
     }
 }
 
@@ -52,19 +48,4 @@ class WordFile extends ResourceFile {
 // Extract WORD.
 // Extract PPT.
 public class ToolApplication {
-    public static void main(String[] args) {
-        List<ResourceFile> resourceFiles = listAllResourceFiles(args[0]);
-        for (ResourceFile resourceFile : resourceFiles) {
-            resourceFile.extract2txt();
-        }
-    }
-
-    private static List<ResourceFile> listAllResourceFiles(String resourceDirectory) {
-        List<ResourceFile> resourceFiles = new ArrayList<>();
-// 파일 형식에 따라 Factory 메서드 방식으로 객체(PdfFile，WordFile, PPTFile) 생성 후， // resourceFiles에 추가
-        resourceFiles.add(new PdfFile("a.pdf"));
-        resourceFiles.add(new WordFile("b.word"));
-        resourceFiles.add(new PPTFile("c.ppt"));
-        return resourceFiles;
-    }
 }
--- src/test/java/pe/msbaek/learningtest/design_patterns/visitor/ToolApplicationTest.java
+++ src/test/java/pe/msbaek/learningtest/design_patterns/visitor/ToolApplicationTest.java
@@ -17,31 +17,7 @@ class ToolApplicationTest {
 
         Extractor extractor = new Extractor();
         for (ResourceFile resourceFile : resourceFiles) {
-//            extractor.extract2txt(resourceFile); // compile error
-            /**
-             * 메소드 오버로딩을 기반으로 한 설계에 대해 설명합니다.
-             * <p>
-             * 이 설계 방식은 다음 두 가지 주요 개념에 기반합니다:
-             * <ol>
-             *     <li><b>다형성:</b> 실행 시간에 객체의 실제 타입을 가져와 실제 타입에 해당하는 메소드를 실행하는 동적 바인딩을 의미합니다.</li>
-             *     <li><b>함수 오버로딩:</b> 정적 바인딩의 일종으로, 컴파일 시에는 객체의 실제 타입을 알 수 없지만 선언된 유형에 해당하는 메소드가 실행됩니다.</li>
-             * </ol>
-             * <p>
-             * 컴파일 오류를 피하기 위해 타입 변환을 수행해야 할 수도 있습니다.
-             */
-            switch (resourceFile) {
-                case PdfFile pdf:
-                    extractor.extract2txt(pdf);
-                    break;
-                case WordFile word:
-                    extractor.extract2txt(word);
-                    break;
-                case PPTFile ppt:
-                    extractor.extract2txt(ppt);
-                    break;
-                default:
-                    throw new IllegalArgumentException("Unknown type: " + resourceFile);
-            }
+            resourceFile.accept(extractor);
         }
     }
 }
